55fd6159cf91d3b6be1cec2c12a61bfb2b889130
Rao Luca <lucara8@gmail.com>
2025-11-15 11:54:45 +0100

    modificato launchfil

---
+++ launch/ex4_launch.py
@@ -0,0 +1,45 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+
+def generate_launch_description():
+    
+    # Launch for tutor's launchfile 
+    ir_launch_package_share = get_package_share_directory('ir_launch')
+
+    exercise_4_launch_file = os.path.join(
+        ir_launch_package_share,
+        'launch',
+        'exercise_4.launch.py'
+    )
+
+    include_exercise_4_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(exercise_4_launch_file)
+    )
+    
+    #Launch for the server
+    turtlenode_server = Node(
+        package='group14_ex4',
+        executable='turtlenode_server',  # <-- This must match your CMakeLists.txt executable name
+        name='turtlenode_server',
+        output='screen'  # Shows print/log statements in the terminal
+    )
+
+    #Launch for the clinet
+    burrow_client = Node(
+        package='group14_ex4',
+        executable='burrow_client',  # <-- This must match your CMakeLists.txt executable name
+        name='burrow_client',
+        output='screen'
+    )
+
+    # --- 3. Return the LaunchDescription ---
+    return LaunchDescription([
+        include_exercise_4_launch,
+        turtlenode_server,
+        burrow_client
+    ])
+
+
