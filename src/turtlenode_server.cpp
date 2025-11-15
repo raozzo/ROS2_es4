bea68e2674847b6731d3ef2d3fe6c4018d93a621
Rao Luca <lucara8@gmail.com>
2025-11-15 16:19:06 +0100

    server libraries

---
+++ src/turtlenode_server.cpp
@@ -0,0 +1,10 @@
+#include "rclcpp/rclcpp.hpp"
+#include <memory>
+#include <vector>
+#include <cmath>
+
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "turtle_interfaces/srv/find_apples.hpp"
+
